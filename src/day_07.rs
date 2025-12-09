009d013a9e66c36a6077494bad1fc3bc6ca51950
Tristan Collis <tristan.collis@gmail.com>
2025-12-08 18:13:04 -0800

    Days 7 and 8

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,9 +1,8 @@
 use common::*;
+use ndarray::{Array2, ArrayBase, Dim, OwnedRepr};
 
-const EXAMPLE_PATH: &str =
-    r"C:/Users/trist/Documents/Github/AdventOfCode/aoc_2025\./day/07\./example.txt";
-const INPUT_PATH: &str =
-    r"C:/Users/trist/Documents/Github/AdventOfCode/aoc_2025\./day/07\./input.txt";
+const EXAMPLE_PATH: &str = r"./day/07/example.txt";
+const INPUT_PATH: &str = r"./day/07/input.txt";
 
 pub fn part_1(use_example: bool) -> Result<()> {
     _part_1(format_input(parse_file(if use_example {
@@ -21,16 +20,106 @@ pub fn part_2(use_example: bool) -> Result<()> {
     })?))
 }
 
-type Input = ();
-
-fn _part_1(_input: Input) -> Result<()> {
-    todo!();
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Cell {
+    Source,
+    Splitter,
+    Empty,
 }
 
-fn _part_2(_input: Input) -> Result<()> {
-    todo!()
+type Input = ArrayBase<OwnedRepr<Cell>, Dim<[usize; 2]>, Cell>;
+
+fn _part_1(input: Input) -> Result<()> {
+    let mut beams = vec![false; input.shape()[1]];
+    beams[input
+        .row(0)
+        .iter()
+        .position(|cell| cell == &Cell::Source)
+        .unwrap()] = true;
+
+    let mut splits = 0;
+
+    for i in 1..input.shape()[0] {
+        let mut next_beams = vec![false; beams.len()];
+
+        for (j, (&beam, &cell)) in beams.iter().zip(input.row(i)).enumerate() {
+            if beam && cell == Cell::Splitter {
+                splits += 1;
+
+                if 0 < j {
+                    next_beams[j - 1] = true;
+                }
+                if j < beams.len() - 1 {
+                    next_beams[j + 1] = true;
+                }
+            } else {
+                next_beams[j] |= beam;
+            }
+        }
+        beams = next_beams;
+    }
+
+    dbg!(splits);
+
+    Ok(())
 }
 
-fn format_input(_input: Vec<String>) -> Input {
-    todo!()
+fn _part_2(input: Input) -> Result<()> {
+    let mut possible_timelines = vec![0; input.shape()[1]];
+    possible_timelines[input
+        .row(0)
+        .iter()
+        .position(|&cell| cell == Cell::Source)
+        .unwrap()] = 1;
+
+    for i in 1..input.shape()[0] {
+        let mut next_timelines = vec![0; possible_timelines.len()];
+
+        for (j, (&timelines, &cell)) in possible_timelines.iter().zip(input.row(i)).enumerate() {
+            if cell == Cell::Splitter {
+                if 0 < j {
+                    next_timelines[j - 1] += timelines;
+                }
+                if j < next_timelines.len() - 1 {
+                    next_timelines[j + 1] += timelines;
+                }
+            }
+            next_timelines[j] += timelines;
+        }
+
+        for (j, &cell) in input.row(i).iter().enumerate() {
+            if cell == Cell::Splitter {
+                next_timelines[j] = 0;
+            }
+        }
+
+        possible_timelines = next_timelines;
+    }
+    let total_timelines: u64 = possible_timelines.iter().sum();
+    dbg!(total_timelines);
+
+    Ok(())
+}
+
+fn format_input(input: Vec<String>) -> Input {
+    let arr = Array2::from_shape_vec(
+        (input.len() / 2, input[0].len()),
+        input
+            .into_iter()
+            .step_by(2)
+            .flat_map(|line| {
+                line.chars()
+                    .map(|c| match c {
+                        'S' => Cell::Source,
+                        '^' => Cell::Splitter,
+                        '.' => Cell::Empty,
+                        _ => panic!(),
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect(),
+    )
+    .unwrap();
+
+    arr
 }
--- src/day_08.rs
+++ src/day_08.rs
@@ -1,9 +1,15 @@
-use common::*;
+use std::{
+    collections::HashSet,
+    ops::{Add, Mul, Sub},
+};
 
-const EXAMPLE_PATH: &str =
-    r"C:/Users/trist/Documents/Github/AdventOfCode/aoc_2025\./day/08\./example.txt";
-const INPUT_PATH: &str =
-    r"C:/Users/trist/Documents/Github/AdventOfCode/aoc_2025\./day/08\./input.txt";
+use common::*;
+use itertools::iproduct;
+use ndarray::Zip;
+use num_traits::Zero;
+
+const EXAMPLE_PATH: &str = r"./day/08/example.txt";
+const INPUT_PATH: &str = r"C:./day/08/input.txt";
 
 pub fn part_1(use_example: bool) -> Result<()> {
     _part_1(format_input(parse_file(if use_example {
@@ -21,16 +27,101 @@ pub fn part_2(use_example: bool) -> Result<()> {
     })?))
 }
 
-type Input = ();
+type Input = Vec<([i64; 3], [i64; 3])>;
 
-fn _part_1(_input: Input) -> Result<()> {
-    todo!();
+fn _part_1(mut input: Input) -> Result<()> {
+    let mut circuits: Vec<HashSet<[i64; 3]>> = vec![];
+
+    for _ in 0..1000 {
+        let (a, b) = input.pop().unwrap();
+
+        let circuit_a = get_circuit(a, &mut circuits);
+        let circuit_b = get_circuit(b, &mut circuits);
+
+        circuits.push(HashSet::from_iter(
+            circuit_a.into_iter().chain(circuit_b.into_iter()),
+        ));
+    }
+
+    circuits.sort_by_key(|circuit| circuit.len());
+
+    dbg!(&circuits[circuits.len() - 3..]
+        .iter()
+        .map(|circuit| circuit.len())
+        .product::<usize>());
+
+    Ok(())
 }
 
-fn _part_2(_input: Input) -> Result<()> {
-    todo!()
+fn _part_2(mut input: Input) -> Result<()> {
+    let mut last_connected_pair: ([i64; 3], [i64; 3]) = input[2];
+
+    let mut circuits: Vec<HashSet<[i64; 3]>> = vec![];
+    while let Some((a, b)) = input.pop() {
+
+        let circuit_a = get_circuit(a.clone(), &mut circuits);
+        if circuit_a.contains(&b) {
+            circuits.push(circuit_a);
+            continue;
+        }
+
+        let circuit_b = get_circuit(b.clone(), &mut circuits);
+
+        circuits.push(HashSet::from_iter(
+            circuit_a.into_iter().chain(circuit_b.into_iter()),
+        ));
+
+        last_connected_pair = (a, b);
+    }
+
+    dbg!(last_connected_pair.0[0] * last_connected_pair.1[0]);
+
+    Ok(())
 }
 
-fn format_input(_input: Vec<String>) -> Input {
-    todo!()
+fn format_input(input: Vec<String>) -> Input {
+    let points: Vec<[i64; 3]> = input
+        .into_iter()
+        .map(|line| match line.split(",").collect::<Vec<_>>()[..] {
+            [x, y, z] => [x.parse().unwrap(), y.parse().unwrap(), z.parse().unwrap()],
+            _ => panic!(),
+        })
+        .collect::<Vec<_>>();
+
+    {
+        let mut pairs: Vec<([i64; 3], [i64; 3])> = iproduct!(points.clone(), points)
+            .filter(|(a, b)| a != b)
+            .step_by(2)
+            .collect();
+
+        pairs.sort_by_key(|(a, b)| -distance_squared(a, b));
+        pairs
+    }
+}
+
+fn distance_squared<T: Mul<Output = T> + Add + Sub<Output = T> + Copy + Zero>(
+    a: &[T; 3],
+    b: &[T; 3],
+) -> T {
+    Zip::from(a)
+        .and(b)
+        .map_collect(|&a_elem, &b_elem| {
+            let diff = a_elem - b_elem;
+            diff * diff
+        })
+        .sum()
+}
+
+fn get_circuit(point: [i64; 3], circuits: &mut Vec<HashSet<[i64; 3]>>) -> HashSet<[i64; 3]> {
+    for i in 0..circuits.len() {
+        if circuits[i].contains(&point) {
+            let mut circuit = circuits.swap_remove(i);
+            circuit.insert(point);
+            return circuit;
+        }
+    }
+
+    let mut circuit = HashSet::new();
+    circuit.insert(point);
+    return circuit;
 }
